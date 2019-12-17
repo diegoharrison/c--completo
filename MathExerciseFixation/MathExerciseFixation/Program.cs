21cd335aac7d2280bd146fb7a86998d3242fba3f
Diego Harrison <diegoharrisonsm@gmail.com>
2019-12-17 16:49:38 -0300

    Math

---
+++ MathExerciseFixation/MathExerciseFixation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MathExerciseFixation {
+    class Program {
+        static void Main(string[] args) {
+
+            double a = 1.0, b = -3.0, c = -4.0;
+            
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(delta);
+
+            Console.WriteLine(x1);
+
+            Console.WriteLine(x2);
+
+
+
+        }
+    }
+}
