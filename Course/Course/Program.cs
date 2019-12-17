307166c4a14dae0ec9ad54663deeda8d610f966a
Diego Harrison <diegoharrisonsm@gmail.com>
2019-12-17 15:30:15 -0300

    Continuation

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -7,7 +8,7 @@ namespace Course
         static void Main(string[] args)
         {
             bool completo = false;
-            char genero = 'M';
+            
             char letra = 'a';
             byte n1 = 255;
             int n2 = 12312;
@@ -16,10 +17,31 @@ namespace Course
             float n5 = 2.3f;
             double n6 = 4.6;
             SByte x = 100;
+
+            char genero = 'M';
+            int idade = 28;
+            double saldo = 10.3578;
             string nome = "Diego";
-            
-            Console.WriteLine(completo);
+
             Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); //Formatação para ponto
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo .ToString("F2", CultureInfo.InvariantCulture)  + " reais");
+
+
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine(completo);            
             Console.WriteLine(letra);
             Console.WriteLine(n1);
             Console.WriteLine(n2);
@@ -28,7 +50,7 @@ namespace Course
             Console.WriteLine(n5);
             Console.WriteLine(n6);
             Console.WriteLine(x);
-            Console.WriteLine(nome);
+           
         }
     }
 }
