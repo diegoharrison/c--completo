327c5e8679fe164f1c18e2e72e3faf8838ffb15f
Diego Harrison <diegoharrisonsm@gmail.com>
2019-12-17 14:54:22 -0300

    First commit

---
+++ Course/Course/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'M';
+            char letra = 'a';
+            byte n1 = 255;
+            int n2 = 12312;
+            int n3 = 12356778;
+            int n4 = 1231;
+            float n5 = 2.3f;
+            double n6 = 4.6;
+            SByte x = 100;
+            string nome = "Diego";
+            
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(x);
+            Console.WriteLine(nome);
+        }
+    }
+}
